d33688330d10b9356ff04d99123b291e7988a930
Louis C-H <louisclementharris@yahoo.co.uk>
2025-03-22 15:55:10 +0200

    Update notes app for middleware and align to example code.

--- part3/P3Notes/index.js
+++ part3/P3Notes/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
-
 let notes = [
   {
     id: "1",
@@ -20,6 +18,18 @@ let notes = [
     important: true
   }
 ]
+
+// Middleware
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
 
 // Home page
 app.get('/', (request, response) => {
@@ -78,6 +88,13 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end()
 })
+
+// Unknown Endpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
