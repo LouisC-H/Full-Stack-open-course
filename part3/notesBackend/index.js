1243ec327c6ff5a54e61e0cf4f51690735b6df35
Louis C-H <louisclementharris@yahoo.co.uk>
2025-03-25 17:24:06 +0200

    Integrated frontend with backend

--- part3/notesBackend/index.js
+++ part3/notesBackend/index.js
@@ -33,6 +33,7 @@ const requestLogger = (request, response, next) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
 
 // Home page
 app.get('/', (request, response) => {
