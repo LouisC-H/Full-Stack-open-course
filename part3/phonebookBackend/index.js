8058cb84af7744e3b1a1bffc4339d7fdcf377c16
Louis C-H <louisclementharris@yahoo.co.uk>
2025-04-02 14:38:55 +0300

    3.15-3.18 working locally

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -6,8 +6,9 @@ const Person = require('./models/person')
 const app = express()
 
 // Middleware
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
+
 
 morgan.token('personPOST', function (req, res) {
   const body = req.body
@@ -26,7 +27,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
     skip: function (req, res) { return req.method !== "POST" }
 }))
 
-// Home page - NOT YET RE-IMPLEMENTED TO USE MONGODB
+//Routes:
+// Home page
 const homePageString = (dateString, people) => {
   numPeople = Object.keys(people).length
   return `Phonebook has info for ${numPeople} people`+ "\n\n" + `${dateString}`
@@ -48,31 +50,31 @@ app.get('/api/persons', (request, response) => {
 })
 
 // Find
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
-// Post- NOT YET RE-IMPLEMENTED TO USE MONGODB
+// Post
 app.post('/api/persons', (request, response) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  if (!body.name || !body.number) {
+  if (!name || !number) {
     return response.status(400).json({ 
       error: 'name or number is missing' 
     })
   }
 
-  // if (phoneBook.find(person => person.name === body.name)){
-  //   return response.status(400).json({ 
-  //     error: 'name must be unique' 
-  //   })
-  // }
-
   const person = new Person({
-    name: body.name,
-    number: body.number,
+    name: name,
+    number: number,
   })
 
   person.save().then(savedPerson =>
@@ -80,11 +82,33 @@ app.post('/api/persons', (request, response) => {
   )
 })
 
-// Delete- NOT YET RE-IMPLEMENTED TO USE MONGODB
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  phoneBook = phoneBook.filter(person => person.id !== id)
-  response.status(204).end()
+// Delete
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+// Put
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
 })
 
 // Unknown Endpoint
@@ -93,6 +117,20 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+// Error handling:
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
