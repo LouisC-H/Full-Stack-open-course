29d9768ca82ba7248a9c54b1d802cc5a7ae3e569
Louis C-H <louisclementharris@yahoo.co.uk>
2025-04-07 18:05:38 +0300

    Added logger and middleware from notes backend.

--- part4/blogs/index.js
+++ part4/blogs/index.js
@@ -1,20 +1,28 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
 const app = express()
 
 const mongoUrl = config.MONGODB_URI
 
-console.log('connecting to', mongoUrl)
+logger.info('connecting to', mongoUrl)
 
 mongoose.connect(mongoUrl)
 
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 const PORT = config.PORT || 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
