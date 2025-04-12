7d642c82f3c2ed6c2d8e762d2510aa796157ee24
Louis C-H <louisclementharris@yahoo.co.uk>
2025-04-12 20:08:48 +0300

    4.5 completed

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -10,7 +10,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (currentFav, blog) => {
+    console.log('currentFav.likes : ', currentFav.likes)
+    if (!currentFav.likes || currentFav.likes < blog.likes) {
+      return blog
+    } else {
+      return currentFav
+    }
+  }
+
+  return blogs.reduce(reducer, {})
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
