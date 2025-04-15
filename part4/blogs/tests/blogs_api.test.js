be566cd9869e4c233963f0ea2a8ef900f6d9fb1c
Louis C-H <louisclementharris@yahoo.co.uk>
2025-04-15 16:05:04 +0300

    4.8 complete

---
+++ part4/blogs/tests/blogs_api.test.js
@@ -0,0 +1,29 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('all blogs are returned, and in the expected format', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
