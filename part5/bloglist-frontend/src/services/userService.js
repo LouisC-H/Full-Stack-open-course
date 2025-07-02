15087ff0525bcc80898061729639af9e129f6844
Louis C-H <louisclementharris@yahoo.co.uk>
2025-07-02 15:09:40 +0300

    Removed forgotten logs

--- part5/bloglist-frontend/src/services/userService.js
+++ part5/bloglist-frontend/src/services/userService.js
@@ -2,10 +2,7 @@ import axios from 'axios'
 const baseUrl = '/api/users'
 
 const getUser = async (id) => {
-  console.log('id : ', id);
-  console.log('`${baseUrl}/${id}` : ', `${baseUrl}/${id}`);
   const request = await axios.get(`${baseUrl}/${id}`)
-  console.log('request : ', request);
   return request.data
 }
 
