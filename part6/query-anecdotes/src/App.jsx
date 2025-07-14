d673af53c0c70545ed583afbc15ad5a54a86a067
Louis C-H <louisclementharris@yahoo.co.uk>
2025-07-14 11:34:33 +0300

    6.20 completed

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,51 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { getNotes } from '../requests/requests'
+
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+
+  const anecdotes = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getNotes,
+    retry: 1
+  })
+  console.log(JSON.parse(JSON.stringify(anecdotes)))
+
+  if ( anecdotes.isLoading ) {
+    return <div>loading data...</div>
+  } else if ( anecdotes.isError ) {
+    return <div>anecdote service not available due to problems in server...</div>
+  }
+  
+  const anecdotesList = anecdotes.data
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotesList.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
